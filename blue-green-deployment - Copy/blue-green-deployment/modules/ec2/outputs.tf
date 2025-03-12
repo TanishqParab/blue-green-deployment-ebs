0bc4a38ff9916c5b45a588151cd8441a8c8e4f98
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-03-12 14:03:41 +0530

    Delete blue-green-deployment - Copy directory

--- blue-green-deployment - Copy/blue-green-deployment/modules/ec2/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "blue_instance_ip" {
-  value = aws_instance.blue.public_ip
-}
-
-output "green_instance_ip" {
-  value = aws_instance.green.public_ip
-}
--- blue-green-deployment - Copy/blue-green-deployment/modules/vpc/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "vpc_cidr" {}
-variable "public_subnet_cidrs" {}
-variable "availability_zones" {}
