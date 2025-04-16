5bc5be46134b3c6c231b70f0c501708c3a2784c0
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-04-16 14:28:42 +0530

    Update app.py

--- blue-green-deployment/modules/elastic_beanstalk/scripts/app.py
+++ blue-green-deployment/modules/elastic_beanstalk/scripts/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Blue-Green Deployment on Amazon Linux 2! Tanishq here New Version v2 v3 v5"
+    return "Hello, Blue-Green Deployment on Amazon Linux 2! Tanishq here New Version v2 v3 v5 v6"
 
 @app.route('/health')
 def health():
