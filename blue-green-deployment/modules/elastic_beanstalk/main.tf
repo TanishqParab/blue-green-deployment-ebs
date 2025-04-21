f9ab7b2af7311d1d1c011d89975cabb1a87635aa
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-04-21 20:09:15 +0530

    Update main.tf

--- blue-green-deployment/modules/elastic_beanstalk/main.tf
+++ blue-green-deployment/modules/elastic_beanstalk/main.tf
@@ -445,7 +445,7 @@ resource "aws_elastic_beanstalk_environment" "green" {
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "SecurityGroups"
-    value = var.ec2_sg_id
+    value     = var.ec2_sg_id != null ? var.ec2_sg_id : module.security_group.ec2_sg_id
   }
 
   tags = {
