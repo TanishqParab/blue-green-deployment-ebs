4d55e8ec2bce4879e7fb60aaad4611f26202a0fc
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-04-23 15:03:49 +0530

    Update outputs.tf

--- blue-green-deployment/modules/elastic_beanstalk/outputs.tf
+++ blue-green-deployment/modules/elastic_beanstalk/outputs.tf
@@ -7,3 +7,8 @@ output "green_environment_url" {
   description = "URL of the Green Elastic Beanstalk environment"
   value       = aws_elastic_beanstalk_environment.green.endpoint_url
 }
+
+// In the eb module
+output "ec2_sg_id" {
+  value = var.ec2_sg_id
+}
