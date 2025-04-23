f30dd715444c41c869c760344ae7e9dc5aa6cfa0
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-04-23 17:02:31 +0530

    Update variables.tf

--- blue-green-deployment/modules/elastic_beanstalk/variables.tf
+++ blue-green-deployment/modules/elastic_beanstalk/variables.tf
@@ -136,3 +136,10 @@ variable "green_target_group_arn" {
   type        = string
   default     = null
 }
+
+variable "alb_name" {
+  description = "The name of the Application Load Balancer"
+  type        = string
+  default     = "blue-green-alb"  # You can set a default value or leave it empty to make it required
+}
+
