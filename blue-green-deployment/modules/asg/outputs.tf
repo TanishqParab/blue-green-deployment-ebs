6981e79ff70527987f2b01a9dce7722c51a41daa
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-06-06 15:06:07 +0530

    Delete blue-green-deployment directory

--- blue-green-deployment/modules/asg/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "asg_id" {
-  value = aws_autoscaling_group.blue_green_asg.id
-}
-
-output "asg_name" {
-  description = "The name of the Auto Scaling Group"
-  value       = aws_autoscaling_group.blue_green_asg.name
-}
-
